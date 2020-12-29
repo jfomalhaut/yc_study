8ad90a913e2a0b23bc209d03bb7a05dac4d8162f
이준 <dlwnssla@jfomalhaut.com>
2020-12-29 22:05:27 +0900

    dd

---
+++ reactlj/start/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Page1 from './routers/Page1';
+import Page2 from './routers/Page2';
+
+ReactDOM.render(<Page1 />, document.getElementById('root'));
