981dde4ce2e1fdaae1c53336978c8440bdaab166
netrro <evergreen_007@daum.net>
2021-01-05 20:28:20 +0900

    dd

---
+++ reactyc/start/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Page2 from './routers/Page2';
+
+ReactDOM.render(<Page2 />, document.getElementById('root'));
---
+++ reactyc/start/src/routers/Page1.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Page1 = () => {
+    return (
+        <h1>Page1 Component</h1>
+    );
+};
+
+export default Page1;
